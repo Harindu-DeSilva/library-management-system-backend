50139420fa085bd7b191aa9a37e6cb35fd2a37d2
Harindu De Silva <harindudesilva12@gmail.com>
2025-12-19 16:54:08 +0530

    created authorizeRoles function to restrict user functionalities based on user role

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -30,4 +30,15 @@ exports.verifyToken = async (req,res,next) => {
     return res.status(401).json({ message: 'Unauthorized: Invalid token', error: err.message });
   }
 
-}
+};
+
+
+
+exports.authorizeRoles = (...roles) => (req,res,next) => {
+
+  if(!roles.includes(req.user.role)) {
+    return res.status(403).json({message: 'Forbidden: You do not have access'});
+  }
+
+  next();
+};
