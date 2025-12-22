edbc70d86d1a3ab2aaf84a0cda0c0f3a4ed66d80
Harindu De Silva <harindudesilva12@gmail.com>
2025-12-22 11:54:10 +0530

    spelling error corrected

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const authRotes = require('./routes/auth.routes');
+const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use('/api/auth', authRotes);
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req,res) => {
   res.status(200).json({message: 'Library Management Backend Running...'});
